36a0e67d0ebe6b7b0cd919fcf5a62390dd9dac16
Gagan Deep Singh <gagangulyanig@gmail.com>
2020-05-13 19:30:25 +0530

    Update Solution for Problem 1
    for preventing unwanted execution while import

--- Problem 1/solution.py
+++ Problem 1/solution.py
@@ -3,12 +3,17 @@
     the multiples of 3 or 5 below 1000.
 """
 
-n = 1000
-sum_ = 0
 
-for i in range(2, n):
-    if i % 3 == 0 or i % 5 == 0:
-        # print(i)
-        sum_ += i
-        
-print(sum_)
+def solution(n):
+    sum_ = 0
+
+    for i in range(2, n):
+        if i % 3 == 0 or i % 5 == 0:
+            # print(i)
+            sum_ += i
+
+    return sum_
+
+
+if __name__ == '__main__':
+    print(solution(1000))
--- Problem 1/solution_optimized.py
+++ Problem 1/solution_optimized.py
@@ -3,33 +3,38 @@
     the multiples of 3 or 5 below 1000.
 """
 
-def SumOfMultiples(number, range_ = 1000):
+
+def SumOfMultiples(number, range_=1000):
     """
         This function takes in number and range_
         and returns sum of multiples of the number 
         below range_ using Finite Arithmetic Progression
-        
+
         Default Value of range_ is 1000
     """
-    
-    # Decrementing 1 cause Multiples of number 
+
+    # Decrementing 1 cause Multiples of number
     # BELOW that range is required
     range_ -= 1
-    
+
     # 999 // 3 -> 333
     p = range_ // number
-    
+
     # Formula for Finite Arithmetic Progression
-    return (number  * (p * (1 + p))) // 2
+    return (number * (p * (1 + p))) // 2
 
-mul_3 = SumOfMultiples(3)
-mul_5 = SumOfMultiples(5)
 
-# multiple of 3 and 5 also contain
-# multiples of 15 which needs to be removed
-mul_15 = SumOfMultiples(15)
+def solution(range_):
+    mul_3 = SumOfMultiples(3, range_)
+    mul_5 = SumOfMultiples(5, range_)
 
-final_value = mul_3 + mul_5 - mul_15
+    # multiple of 3 and 5 also contain
+    # multiples of 15 which needs to be removed
+    mul_15 = SumOfMultiples(15, range_)
 
-# 233168
-print(final_value)
+    final_value = mul_3 + mul_5 - mul_15
+    return final_value
+
+
+if __name__ == '__main__':
+    print(solution(1000))
